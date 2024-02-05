96577d0b6c59f4cec984d6f61d1b3195f5134db5
syed <syedshaahid02@gmail.com>
2024-02-05 15:03:14 +0530

    updated

--- template.py
+++ template.py
@@ -17,7 +17,7 @@ list_of_files = [
     'src/utils/__init__.py'
     'src/utils/utils.py',
     'src/logger/logging.py',
-    'src/exception/exception',
+    'src/exception/exception.py',
     'tests/unit/__init__.py',
     'tests/integration/__init__.py',
     'init_setup.sh',
@@ -40,3 +40,5 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, 'w') as f:
             pass #create an empty file
+
+            
